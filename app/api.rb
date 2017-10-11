e3cd5197765aafcac785e9b8009fd9de82f353ff
Gregoire Philipon <philipon.gregoire@gmail.com>
2017-10-11 21:36:10 +0200

    exo API done

--- app/api.rb
+++ app/api.rb
@@ -1,10 +1,25 @@
 require 'bundler/setup'
 require 'sinatra/base'
+require 'json'
 
 class Api < Sinatra::Base
 
 	get '/hello' do
-		'hello world!'
+		'Hello world!'
 	end
+
+  get '/movies' do
+    content_type :json
+    a = JSON.parse(File.read('data.json'))
+    a.fetch("movies").to_json
+  end
+
+  get '/movies/:data' do
+    content_type :json
+    a = JSON.parse(File.read('data.json'))
+    b = a.fetch("movies")
+    b.select { |nb| nb["id"] == params[:data].to_i }.to_json
+  end
+
 
 end
